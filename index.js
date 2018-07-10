565286666a7ff6ba8da04c5367fb636437ecf761
Aleksandar-Sasa Calic <sc@payuca.com>
2018-07-10 16:57:31 +0200

    - Save commit.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 const
     express = require('express'),
     bodyParser = require('body-parser'),
@@ -8,19 +10,20 @@ app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
 
+    const VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>";
+
     let body = req.body;
 
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
 
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
+        // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
 
-            // Gets the message. entry.messaging is an array, but
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
 
         // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
@@ -59,3 +62,18 @@ app.get('/webhook', (req, res) => {
         }
     }
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
